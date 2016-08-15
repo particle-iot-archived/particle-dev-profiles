06320658ff6dca3835b1d99ba2408df3d373c38b
Wojtek Siudzinski <admin@suda.pl>
2016-08-15 14:56:57 +0100

    Fix docs script

--- docs.sh
+++ docs.sh
@@ -8,6 +8,6 @@ cd $dir
 node_modules/documentation/bin/documentation.js build src/profile-manager.js --shallow -f html -o $tmpdir
 cd $tmpdir
 git add .
-git commit -m 'Update docs'
-git push -u origin gh-pages
+git -c user.name='Travis CI' -c user.email='bots@particle.io' commit -m 'Update docs'
+git push -f -q https://suda:$GITHUB_API_KEY@github.com/spark/particle-dev-profiles gh-pages &2>/dev/null
 cd $dir
